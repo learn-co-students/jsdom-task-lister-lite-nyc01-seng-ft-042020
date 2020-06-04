50dca8137b8793fe27960d786c8509b457deaa55
Srishti Nath <srishti.nath@gmail.com>
2020-06-03 23:46:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const submitButton = document.querySelector("#create-task-form > input[type=submit]:nth-child(3)")
+  const tasksUL = document.getElementById('tasks')
+
+  function addToList(text) {
+    let listItem = document.createElement('li')
+    let listButton = document.createElement('button')
+    listItem.innerText = text
+    listButton.textContent = "Xs"
+    listButton.id = listItem.innerText
+
+    listItem.append(listButton)
+    tasksUL.append(listItem)
+    listButton.addEventListener('click', function(e){
+      listItem.remove()
+    })
+  }
+
+  // form and .preventdefault
+  submitButton.addEventListener('click', function(e){
+    e.preventDefault()
+    const inputText = document.getElementById('new-task-description').value
+    addToList(inputText)
+  })
 });
