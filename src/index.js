8040b5997f763eacdc599b8edfaae6e2cec9d38f
DarkLaughter <ramon.l.echeverria@gmail.com>
2020-06-06 22:36:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    const taskForm = document.querySelector("#create-task-form")
+    const taskDescripton = document.querySelector("#new-task-description")
+    const taskList = document.querySelector("#tasks")
+
+    const newTask = e =>{
+      e.preventDefault();
+
+      const newTask = document.createElement('li')
+      newTask.innerText = taskDescripton.value
+      
+      updateTaskList(newTask)
+      e.target.reset()
+    }
+
+    const updateTaskList = task => {
+      taskList.appendChild(task)
+    }
+
+    taskForm.addEventListener("submit", newTask);
 });
